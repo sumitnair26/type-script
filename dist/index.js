c3a8495d5fb6ddcc9b8382a06c87bc45a24acb23
sumitnair26 <sumit.nair26@gmail.com>
2023-11-18 13:35:31 +0530

    arrays

--- dist/index.js
+++ dist/index.js
@@ -5,3 +5,7 @@ node --watch dist/index.js
 /*
     // arrays
 */
+let names = ['Sumit', 'Nair', 'Amit'];
+let ages = [25, 33, 44];
+names.push('some new name');
+ages.push(35);
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,10 @@ node --watch dist/index.js
     // arrays
 */
 
+let names : string[] = ['Sumit', 'Nair', 'Amit']
+let ages : number[] = [25,33,44]
+
+names.push('some new name')
+ages.push(35)
+
+
