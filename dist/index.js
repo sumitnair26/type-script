5dd1fa41a36b48bf08bc661ad01bc3c4909aafaa
sumitnair26 <sumit.nair26@gmail.com>
2023-11-18 13:45:56 +0530

    type inference array

--- dist/index.js
+++ dist/index.js
@@ -9,3 +9,10 @@ let names = ['Sumit', 'Nair', 'Amit'];
 let ages = [25, 33, 44];
 names.push('some new name');
 ages.push(35);
+/*
+// type inference with array
+*/
+let fruits = ['apples', 'bananas', 'mangoes'];
+const f = fruits[2];
+let things = [1, true, 'some string'];
+const s = things[2];
--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,16 @@ let ages : number[] = [25,33,44]
 names.push('some new name')
 ages.push(35)
 
+/*
+// type inference with array
+*/
+
+let fruits = ['apples', 'bananas', 'mangoes']
+
+const f = fruits[2]
+
+let things = [1, true, 'some string']
+
+const s = things[2]
+
 
